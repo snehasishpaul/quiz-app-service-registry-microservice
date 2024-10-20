cb49624f1e830442e9c56354f3f6277a7c0623ea
Snehasish Paul <weishenpov@gmail.com>
2024-10-20 12:24:02 +0530

    intial commit
    intial commit

---
+++ src/main/java/com/snehasish/microservice/service_registry/QuizAppServiceRegistryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.snehasish.microservice.service_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizAppServiceRegistryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizAppServiceRegistryMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/snehasish/microservice/service_registry/QuizAppServiceRegistryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snehasish.microservice.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppServiceRegistryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
